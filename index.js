fbc2699e16b2fc403ac4a8667935ba07db977f84
usama.mohsin.3975 <usama.mohsin.3975@slashnext.net>
2021-01-15 19:05:53 +0500

    FizzBuzz

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function max(val1, val2) {
 function isLandscape(width, height) {
     return width > height;
 }
+
+function fizzBuzz(input) {
+    if (typeof input !== 'number') {
+        return NaN;
+    }
+
+    let result = '';
+
+    if (input % 3 === 0) {
+        result += 'Fizz';
+    }
+
+    if (input % 5 === 0) {
+        result += 'Buzz';
+    }
+
+    return result || input;
+}
