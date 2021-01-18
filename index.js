5e4b80283a106acbec25e0774b3a4aba88691805
usama.mohsin.3975 <usama.mohsin.3975@slashnext.net>
2021-01-18 14:57:11 +0500

    sum

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ function fizzBuzz(input) {
     return result || input;
 }
 
+function sum(limit) {
+    let sum = 0;
+
+    for (let number = 0; number <= limit; number++) {
+        if (number % 3 === 0 || number % 5 === 0) {
+            sum+=number;
+        }
+    }
+
+    return sum;
+}
