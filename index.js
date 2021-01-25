c2de15d72750ecc889315ad0329c1d675325eb87
usama.mohsin.3975 <usama.mohsin.3975@slashnext.net>
2021-01-25 15:23:23 +0500

    priceRanges

--- index.js
+++ index.js
@@ -195,3 +195,9 @@ function Post(title, body, author) {
     this.comments = [];
     this.isLive = false;
 }
+
+let priceRanges = [
+    { label: "$", toolTip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 },
+    { label: "$$", toolTip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 },
+    { label: "$$$", toolTip: 'Expensive', minPerPerson: 21, maxPerPerson: 50 }
+];
